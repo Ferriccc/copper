472a390d64fbec9f99e0d3d93a54ae0fbbc425a9
ferriccc <shrey.s4@ahduni.edu.in>
2024-07-27 16:16:53 +0530

    improved tests, added a way to stimulate a templated dir for each test and define tests in toml files

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,286 +1,108 @@
+#include "/home/shrey/copper/external_deps/toml.hpp"
 #include <filesystem>
-#include <fstream>
 #include <iostream>
-#include <vector>
-#include <string>
 
-using namespace std::string_view_literals;
+const char *redColorCode = "\e[1;31m";
+const char *greenColorCode = "\e[1;32m";
+const char *yellowColorCode = "\e[1;33m";
+const char *resetColorCode = "\e[0m";
+
+#define ASSERT(x)                                                                         \
+  if (!(x)) {                                                                             \
+    std::cerr << redColorCode << "CHECK FAILED: " << resetColorCode << #x << std::endl;   \
+    exit(1);                                                                              \
+  }
+
+#define SUCCESS(x) std::cerr << greenColorCode << "WORKS: " << resetColorCode << x << std::endl;
+
+#define DBG(x) std::cerr << yellowColorCode << "DBG: " << resetColorCode << x << std::endl;
+
 namespace fs = std::filesystem;
 
-const std::string GENSTOREDIR = "/home/shrey/copper/tests/.copper";
-const std::string CONFIGDIR = "/home/shrey/copper/tests/dir";
-const std::string SRCDIR = "/home/shrey/copper";
-const std::string COMPILECMD = "make all";
 const std::string OUT = "/home/shrey/copper/tests/out.txt";
-const std::string OUTSUFFIX = " 2> " + OUT;
-
-#define ASSERT(x)                                                              \
-  if (!(x)) {                                                                  \
-    std::cerr << "\033[31mASSERTION FAILED: " << #x << "\033[0m" << std::endl; \
-    exit(1);                                                                   \
-  } else {                                                                     \
-    std::cerr << "\033[32mASSERTION PASSED: " << #x << "\033[0m" << std::endl; \
-  }
+const std::string CMDSUFFIX = " 2> " + OUT;
+const std::string TEMPLATE_DIR = "/home/shrey/copper/tests/containers_template";
+const std::string TESTS_DIR = "/home/shrey/copper/tests/containers";
 
 void compile() {
   int ret = system("(cd /home/shrey/copper/ && make clean test)");
   ASSERT(ret != -1 && WIFEXITED(ret) && WEXITSTATUS(ret) == 0);
+  SUCCESS("it compiles");
 }
 
-void writeToTestConfig(const std::basic_string_view<char> &source) {
-  fs::create_directories(CONFIGDIR);
-  std::ofstream of(CONFIGDIR + "/config.toml");
-  of << source << std::endl;
-  of.close();
-}
-
-void writeToTestConfigOld(const std::basic_string_view<char> &source, const std::string &gen_name) {
-  std::string path = GENSTOREDIR + "/" + gen_name;
-  fs::create_directories(path);
-
-  std::ofstream of(path + "/config.toml");
-  of << source << std::endl;
-  of.close();
-
-}
-
-void writeToInfo(const std::basic_string_view<char> &source) {
-  fs::create_directories(GENSTOREDIR);
-  std::ofstream of(GENSTOREDIR + "/info.toml");
-  of <<  source << std::endl;
-  of.close();
-}
-
-void cleanDirs() {
-    std::error_code ec;    
-    fs::remove_all(GENSTOREDIR, ec);
-    fs::remove_all(CONFIGDIR, ec);
-}
-
-void verifyOutput(const std::vector<std::string> &&exp, bool can_continue = 0) {
-    int i = 0;
-    std::ifstream outfile(OUT);
-    std::string line;
-    while (getline(outfile, line)) {
-      if (!can_continue) {
-        ASSERT(i < exp.size());
-      } else if (i >= exp.size()) {
-        break;
-      }
-      ASSERT(line == exp[i]);
-      ++i;
-    }
-}
-
-void testMain() {
-  { // argc test with empty arg
-    cleanDirs();
-    system(std::string("copper" + OUTSUFFIX).c_str());
-    verifyOutput({
-      "ASSERTION FAILED: argc == 2"
+void verifyFiles(const toml::table &tbl) {
+  if (const toml::array *arr = tbl["exp_files"].as_array()) {
+    arr->for_each([&](const auto &o) {
+      ASSERT(fs::exists(o.value_or(""))); 
     });
   }
-  ASSERT("PASSED testMain 1");
-
-  { // argc test with more args than needed
-    cleanDirs();
-    system(std::string("copper arg1 arg2 arg10" + OUTSUFFIX).c_str());
-    verifyOutput({
-      "ASSERTION FAILED: argc == 2"
-    });
-  } 
-  ASSERT("PASSED testMain 2");
+  DBG("verified exp_files");
 }
 
-void testConstants() {
-  { // providing path with ending '/'
-    cleanDirs();
-    system(std::string("copper /home/shrey/copper/tests/dir/" + OUTSUFFIX).c_str());
-    verifyOutput({
-      "WARN: found '/' at provided path ending"
-    }, 1);
-  }
-  ASSERT("PASSED testConstants 1");
-
-  { // providing non-exsistent path
-    cleanDirs();
-    system(std::string("copper /someNonExsistentPath" + OUTSUFFIX).c_str());
-    verifyOutput({
-      "ASSERTION FAILED: std::filesystem::exists(new_config_path)"
+void verifySymlinks(const toml::table &tbl) {
+  if (const toml::array *arr = tbl["exp_symlinks"].as_array()) {
+    arr->for_each([&](const auto &o) { 
+      ASSERT(fs::is_symlink(o.value_or(""))); 
     });
   }
-  ASSERT("PASSED testConstants 2");
-
-  { // providing empty generation_store_dir in config.toml
-    cleanDirs();
-    writeToTestConfig(R"(
-[copper]
-    )"sv);
-    system(std::string("copper /home/shrey/copper/tests/dir" + OUTSUFFIX).c_str());
-    verifyOutput({
-      "ASSERTION FAILED: genStoreDir != \"\""
-    });
-  }
-  ASSERT("PASSED testConstants 3");
-
-  { // providing generation_store_dir with ending '/'
-    cleanDirs();
-    writeToTestConfig(R"(
-[copper]
-generation_store_dir = "/home/shrey/copper/tests/.copper/"
-    )"sv);
-    system(std::string("copper /home/shrey/copper/tests/dir" + OUTSUFFIX).c_str());
-    verifyOutput({
-      "WARN: found '/' at provided path ending"
-    }, 1);
-  }
-  ASSERT("PASSED testConstants 4");
-
-  { // providing non-exsistent generation_store_dir AND info.toml
-    cleanDirs();
-    writeToTestConfig(R"(
-[copper]
-generation_store_dir = "/home/shrey/copper/tests/.copper"
-    )"sv);
-    system(std::string("copper /home/shrey/copper/tests/dir" + OUTSUFFIX).c_str());
-    verifyOutput({
-      "WARN: generation_store_dir path defined in config.toml does not exists, creating ...",
-      "WARN: info.toml file does not exists, creating ..."
-    }, 1);
-    ASSERT(fs::exists(GENSTOREDIR));
-    ASSERT(fs::exists(GENSTOREDIR + "/info.toml"));
-  }
-  ASSERT("PASSED testConstants 5");
+  DBG("verified exp_symlinks");
 }
 
-void testSystemPackages() {
-  { // no system_packages table
-    cleanDirs();
-    writeToInfo("");
-    writeToTestConfig(R"(
-[copper]
-generation_store_dir = "/home/shrey/copper/tests/.copper"
-    )"sv);
-    system(std::string("copper /home/shrey/copper/tests/dir" + OUTSUFFIX).c_str());
-    verifyOutput({
-      "WARN: new config.toml does not contain \"system_packages\" table, ignoring ...",
-      "WARN: old config.toml does not contain \"system_packages\" table, ignoring ...",
+void verifyOutput(const toml::table &tbl) {
+  std::ifstream outfile(OUT);
+  std::string line;
+  bool canContinue = tbl["continue"].value_or(0);
+
+  if (const toml::array *arr = tbl["exp"].as_array()) {
+    arr->for_each([&](const auto &o) {
+      ASSERT(getline(outfile, line));
+      ASSERT(line == o.value_or(""));
     });
   }
-  ASSERT("PASSED testSystemPackages 1");
 
-  { // empty add / del cmd
-    cleanDirs();
-    writeToInfo("");
-    writeToTestConfig(R"(
-[copper]
-generation_store_dir = "/home/shrey/copper/tests/.copper"
-
-[system_packages]
-    )"sv);
-    system(std::string("copper /home/shrey/copper/tests/dir" + OUTSUFFIX).c_str());
-    verifyOutput({
-      "ASSERTION FAILED: strlen(add_cmd) != 0",
-    });
+  if (!canContinue) {
+    ASSERT(!getline(outfile, line));
   }
-  ASSERT("PASSED testSystemPackages 2");
 
-  { // only new config and no old
-    cleanDirs();
-    writeToTestConfigOld(R"(
-[copper]
-generation_store_dir = "/home/shrey/copper/tests/.copper"
+  outfile.close();
 
-[system_packages]
-add_cmd = "ADD_PACK #1"
-del_cmd = "DEL_PACK #1"
-                         )"sv,
-                         "gen1");
-    writeToInfo(R"(
-current = "/home/shrey/copper/tests/.copper/gen1"
-                )"sv);
-    writeToTestConfig(R"(
-[copper]
-generation_store_dir = "/home/shrey/copper/tests/.copper"
-
-[system_packages]
-add_cmd = "ADD_PACK #1"
-del_cmd = "DEL_PACK #1"
-list = [
-  "PACKAGE1",
-]
-[system_packages.PACKAGE1]
-add_cmds = [
-  "ADD_PACK_HOOK1 PACKAGE1",
-  "ADD_PACK_HOOK2 PACKAGE1",
-]
-    )"sv);
-    system(std::string("copper /home/shrey/copper/tests/dir" + OUTSUFFIX).c_str());
-    verifyOutput({
-      "SUCCESS: ADD_PACK PACKAGE1",
-      "SUCCESS: ADD_PACK_HOOK1 PACKAGE1",
-      "SUCCESS: ADD_PACK_HOOK2 PACKAGE1"
-    });
-  }
-  ASSERT("PASSED testSystemPackages 3");
-
-  { // both old and new config
-    cleanDirs();
-    writeToTestConfigOld(R"(
-[copper]
-generation_store_dir = "/home/shrey/copper/tests/.copper"
-
-[system_packages]
-add_cmd = "ADD_PACK #1"
-del_cmd = "DEL_PACK #1"
-list = [
-  "PACKAGE1"
-]
-[system_packages.PACKAGE1]
-del_cmds = [
-  "DEL_PACK_HOOK1 PACKAGE1",
-  "DEL_PACK_HOOK2 PACKAGE1",
-]
-                         )"sv,
-                         "gen2");
-    writeToInfo(R"(
-current = "/home/shrey/copper/tests/.copper/gen2"
-                )"sv);
-    writeToTestConfig(R"(
-[copper]
-generation_store_dir = "/home/shrey/copper/tests/.copper"
-
-[system_packages]
-add_cmd = "ADD_PACK #1"
-del_cmd = "DEL_PACK #1"
-list = [
-  "PACKAGE2",
-]
-[system_packages.PACKAGE2]
-add_cmds = [
-  "ADD_PACK_HOOK1 PACKAGE2",
-  "ADD_PACK_HOOK2 PACKAGE2",
-]
-    )"sv);
-    system(std::string("copper /home/shrey/copper/tests/dir" + OUTSUFFIX).c_str());
-    verifyOutput({
-      "SUCCESS: DEL_PACK PACKAGE1",
-      "SUCCESS: DEL_PACK_HOOK1 PACKAGE1",
-      "SUCCESS: DEL_PACK_HOOK2 PACKAGE1",
-      "SUCCESS: ADD_PACK PACKAGE2",
-      "SUCCESS: ADD_PACK_HOOK1 PACKAGE2",
-      "SUCCESS: ADD_PACK_HOOK2 PACKAGE2",
-    });
-  }
-  ASSERT("PASSED testSystemPackages 4");
+  DBG("verified exp")
 }
 
-int main() {
+void tester(std::string &path) {
+  const toml::table &tbl = toml::parse_file(path + "/test.toml");
+
+  const std::string name = tbl["name"].value_or("");
+
+  std::string run_cmd = "copper";
+  if (const toml::array* arr = tbl["args"].as_array()) {
+    arr->for_each([&](const auto &arg) {
+      run_cmd += " " + std::string(arg.value_or(""));
+    });
+  }
+
+  run_cmd += CMDSUFFIX;
+
+  int ret = system(run_cmd.c_str());
+
+  verifyOutput(tbl);
+  verifyFiles(tbl);
+  verifySymlinks(tbl);
+
+  SUCCESS(name);
+}
+
+int main(int argc, char** argv) {
+  std::error_code ec;
+  fs::remove_all(TESTS_DIR, ec);
+  fs::copy(TEMPLATE_DIR, TESTS_DIR, fs::copy_options::recursive);
+
   compile();
 
-  testMain();
-  testConstants();
-  testSystemPackages();
+  int cnt = std::stoi(argv[1]);
 
-  cleanDirs();
+  for (int i = 1; i <= cnt; ++i) {
+    std::string path = TESTS_DIR + "/c" + std::to_string(i);
+    tester(path);
+  }
 }
